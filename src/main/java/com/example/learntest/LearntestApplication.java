7d6089563bf0c97f376b9cff38aa995480f9a77d
zhazhahehedian <zhazhahehedian@aliyun.com>
2020-02-12 11:47:41 +0800

    合并

---
+++ src/main/java/com/example/learntest/LearntestApplication.java
@@ -0,0 +1,13 @@
+package com.example.learntest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearntestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LearntestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/learntest/LearntestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.learntest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearntestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
